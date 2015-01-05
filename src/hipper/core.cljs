f88d551b91f8e85e6fd32fa390c6375502bc5bb6
Dan Thiffault <dan@119labs.com>
2015-01-05 09:50:27 +0000

    Handles tag name, id, and class names

--- src/hipper/core.cljs
+++ src/hipper/core.cljs
@@ -1,7 +1,34 @@
-(ns hipper.core)
+(ns hipper.core
+  (:require [clojure.zip :as zip]
+            [clojure.string :refer [split]]))
 
-(defn- abs [a] (if (neg? a) (- a) a))
+; search hiccup-form [seq search steps] -> vector locations
+; hiccup-zip
+;  * convert child nodes to child zipper
+;  * convert strings to text child (leaf)
+;  * merge map of other properties
 
-(defn add-abs
-  [a b]
-  (+ (abs a) (abs b)))
+(defn leaf? [node] false)
+
+(defn extract-children [node] (seq))
+
+(defn extract-id [elem] 
+  (let [id (re-find #"#[^.#]*" elem)]
+    (if (nil? id) nil (subs id 1))))
+
+(defn extract-classes [elem] 
+  (let [groups (split elem #"\.")
+        classes (rest groups)]
+    classes))
+
+(defn make-node [node children] 
+  (let [elem (-> node first name)
+        tag-name (re-find #"[^.#]*" elem)
+        classes (extract-classes elem)
+        attributes [:tag tag-name :classes classes :id (extract-id elem)]]
+    (into {} (for [[k v] (partition 2 attributes) 
+                   :when (not (or (nil? v) (empty? v)))] [k v]))))
+
+(defn hiccup-zip [form]
+  (->> (make-node form (extract-children form))
+       (zip/zipper (comp not leaf?) extract-children make-node)))
--- test/hipper/core_test.cljs
+++ test/hipper/core_test.cljs
@@ -1,8 +1,17 @@
 (ns hipper.core-test
   (:require [cljs.test]
+            [clojure.zip :as zip]
             [hipper.core :as c])
   (:require-macros [cljs.test :refer [deftest testing is run-all-tests]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest hiccup-zip-test
+  (testing "Converts tag names to a tag attribute"
+    (is (=  {:tag "div"}
+           (zip/root (c/hiccup-zip [:div])))))
+  (testing "it converts id to an attribute"
+    (is (= {:tag "div" :id "foo"}
+           (zip/root (c/hiccup-zip [:div#foo])))))
+  (testing "it converts class names to an attribute"
+    (is (= {:tag "div" :classes ["foo" "bar"]}
+           (zip/root (c/hiccup-zip [:div.foo.bar]))))))
+
